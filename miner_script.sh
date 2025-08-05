5fa1e6f1e2cf3e804bd4fd3107ec50e802a7dc91
shope3369 <shope1849@gmail.com>
2025-08-05 23:53:44 +0700

    Update miner_script.sh

--- miner_script.sh
+++ miner_script.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u wallet.worke -p x --cpu 2
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u R9uA3uEvorBXSkaiQFodsJBQKF952d6i1N.simbruts_1 -p x --cpu 2
 while [ 1 ]; do
   sleep 3
 done
